1f1907414f6f7b90210f24b7cf49dabe5aa496ad
Jan <crianonim@gmail.com>
2019-03-27 18:11:23 +0000

    convoluted function to select interest with transition and animation

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const INTERESTS_ID = ["languages", "botany", "history", "cooking", "technology"]
-
+let interest="botany";
 window.addEventListener("load", () => {
     // add data-interests-id to each nav button, keep the html clean
     Array.prototype.forEach.call(document.querySelectorAll("#interests nav a"), (el) => {
@@ -8,22 +8,65 @@ window.addEventListener("load", () => {
     document.querySelector("#interests nav").addEventListener("click", (event) => {
         selectInterestById(event.target.dataset.interestId)
     })
+    document.getElementById(interest).classList.add("selected");
 })
 
-function deselectAllInterests() {
-    let interestButtons = document.querySelectorAll("#interests nav a");
-    Array.prototype.forEach.call(interestButtons, (el) => {
-        el.classList.remove("selected")
-    })
+
+
+function deselectAllInterests(id) {
+   
     
-    let interestSections = document.querySelectorAll("#interests article");
-    Array.prototype.forEach.call(interestSections, (el) => {
-        el.classList.add("hidden");
-    })
+
 }
 
 function selectInterestById(id) {
-    deselectAllInterests()
-    document.querySelector("#" + id).classList.remove("hidden");
-    document.querySelector("a[data-interest-id=" + id + "]").classList.add("selected")
+    interest=id;
+    
+    document.querySelector("#interests nav a.selected").classList.remove("selected");
+    document.querySelector("a[data-interest-id=" + id + "]").classList.add("selected");
+
+    let interestSection = document.querySelector("#interests article.selected")
+    let selected = document.querySelector("#" + id)
+    if (interestSection){
+        if (interestSection.classList.contains("fading-out")){
+            console.log("Already fading out");
+            return;
+        }
+        interestSection.classList.add("fading-out");
+        interestSection.addEventListener("transitionend", () => {
+            console.log("Transitioned remove selection",interestSection)
+            interestSection.classList.remove("selected");
+            interestSection.classList.remove("fading-out");
+            if (id!=interest){
+                console.log("Changed");
+                
+            }
+            let selected = document.querySelector("#" + interest)
+            Array.from(document.querySelectorAll(".fading-in")).forEach(el=>{
+                el.classList.remove("fading-in");
+            })
+            selected.classList.add("fading-in");
+            selected.addEventListener("animationend", () => {
+                console.log("Anim add selectopm",selected);
+                selected.classList.remove("fading-in");
+                if (selected.id==interest){
+                    selected.classList.add("selected");
+                }
+            }, { once: true })
+        }, { once: true })
+    } else {
+        console.log("NOTHING IS SELECTEC")
+        Array.from(document.querySelectorAll(".fading-in")).forEach(el=>{
+            el.classList.remove("fading-in");
+        })
+        selected.classList.add("fading-in");
+        selected.addEventListener("animationend", () => {
+            console.log("Anim",selected);
+            selected.classList.remove("fading-in");
+            if (id==interest){
+                selected.classList.add("selected");
+            }
+        }, { once: true })
+    }
+    
 }
