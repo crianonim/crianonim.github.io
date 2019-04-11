1dfa7763745961c4fe58748e1570ed0248237935
Jan <crianonim@gmail.com>
2019-04-11 07:59:46 +0100

    Selecting starting interest programatically to initiate all effects.

--- main.js
+++ main.js
@@ -1,122 +1,124 @@
-const INTERESTS_ID = ["languages", "botany","cooking", "history",  "technology"];
-const SECTIONS_ID=["who","why","how"];
-let interest="botany";
-let section=0;
+const INTERESTS_ID = ["languages", "botany", "cooking", "history", "technology"];
+const SECTIONS_ID = ["who", "why", "how"];
+let interest = "languages";
+let section = 0;
 let timeOut;
-let paused=false;
+let paused = false;
 
 window.addEventListener("load", () => {
     // add data-interests-id to each nav button, keep the html clean
     Array.prototype.forEach.call(document.querySelectorAll("#interests nav a"), (el) => {
         el.dataset.interestId = el.textContent.toLowerCase();
     })
-    
+
     // Set the header nav button selection based on hash or default to 0
-    if (location.hash){
-        let hashSpecified=location.hash.substring(1);
+    if (location.hash) {
+        let hashSpecified = location.hash.substring(1);
         changeSectionInHeaderNav(SECTIONS_ID.indexOf(hashSpecified))
     } else {
         changeSectionInHeaderNav(0)
     }
 
     document.querySelector("#interests nav").addEventListener("click", (event) => {
-        if (event.target.tagName=="A") selectInterestById(event.target.dataset.interestId)
+        if (event.target.tagName == "A") selectInterestById(event.target.dataset.interestId)
     })
-    document.getElementById(interest).classList.add("selected");
-    
-    
-    window.addEventListener("keydown",(event)=>{
-        if (event.keyCode==37){
+
+    selectInterestById(interest)
+    // document.getElementById(interest).classList.add("selected");
+
+
+    window.addEventListener("keydown", (event) => {
+        if (event.keyCode == 37) {
             selectPreviousInterest();
         }
-        if (event.keyCode==39){
+        if (event.keyCode == 39) {
             selectNextInterest();
         }
     })
-    
-    document.getElementById("pause-button").addEventListener("click",(event)=>{
-        event.target.innerHTML=paused?'&#9208':'&#9654'
+
+    document.getElementById("pause-button").addEventListener("click", (event) => {
+        event.target.innerHTML = paused ? '&#9208' : '&#9654'
         if (paused) resumeCountDown();
         else pauseCountDown();
     })
-    document.getElementById("back-button").addEventListener("click",()=>{
+    document.getElementById("back-button").addEventListener("click", () => {
         selectPreviousInterest();
 
     })
-    document.getElementById("forward-button").addEventListener("click",()=>{
+    document.getElementById("forward-button").addEventListener("click", () => {
         selectNextInterest();
     })
-    window.addEventListener("scroll",(event)=>{
-        let height=window.innerHeight;
-        let scrolled=window.scrollY;
-        let newSection=(scrolled/height)>>0;
-        if (newSection!=section){
-           changeSectionInHeaderNav(newSection);
+    window.addEventListener("scroll", (event) => {
+        let height = window.innerHeight;
+        let scrolled = window.scrollY;
+        let newSection = (scrolled / height) >> 0;
+        if (newSection != section) {
+            changeSectionInHeaderNav(newSection);
         }
     })
     startCountdownToChange();
 })
-function changeSectionInHeaderNav(newSection){
-    let headerElement=document.querySelector('header');
-    let sectionName=SECTIONS_ID[newSection]
-    document.getElementById("nav-"+SECTIONS_ID[section]).classList.remove("selected")
+function changeSectionInHeaderNav(newSection) {
+    let headerElement = document.querySelector('header');
+    let sectionName = SECTIONS_ID[newSection]
+    document.getElementById("nav-" + SECTIONS_ID[section]).classList.remove("selected")
     // console.log("Change to",SECTIONS_ID[newSection]);
-    document.getElementById("nav-"+sectionName).classList.add("selected")
-    headerElement.classList.remove("who","why","how");
+    document.getElementById("nav-" + sectionName).classList.add("selected")
+    headerElement.classList.remove("who", "why", "how");
     headerElement.classList.add(sectionName);
-    
-    section=newSection;
+
+    section = newSection;
 }
-function startCountdownToChange(){
+function startCountdownToChange() {
     clearTimeout(timeOut)
-    timeOut=setTimeout(()=>{
+    timeOut = setTimeout(() => {
         // console.log("Happened!")
-        if (paused){
+        if (paused) {
             // console.log("Paused");
             return;
         }
         selectNextInterest();
-        startCountdownToChange();    
-    },3000);
+        startCountdownToChange();
+    }, 3000);
 }
-function pauseCountDown(){
+function pauseCountDown() {
     clearTimeout(timeOut);
-    paused=true;
+    paused = true;
 }
-function resumeCountDown(){
-    paused=false;
+function resumeCountDown() {
+    paused = false;
     //maybe move to next already?
     startCountdownToChange();
 }
-function selectNextInterest(){
+function selectNextInterest() {
     selectInterestByStep(1);
 }
-function selectPreviousInterest(){
+function selectPreviousInterest() {
     selectInterestByStep(-1);
 }
 
-function selectInterestByStep(step){
-    let interestCount=INTERESTS_ID.length
-    let current=INTERESTS_ID.indexOf(interest);
-    current+=step;
-    if (current>=interestCount) current-=interestCount;
-    if (current<0) current+=interestCount;
+function selectInterestByStep(step) {
+    let interestCount = INTERESTS_ID.length
+    let current = INTERESTS_ID.indexOf(interest);
+    current += step;
+    if (current >= interestCount) current -= interestCount;
+    if (current < 0) current += interestCount;
     selectInterestById(INTERESTS_ID[current])
 
 }
 
-function fadeIn(selected){
-    Array.from(document.querySelectorAll(".fading-in")).forEach(el=>{
+function fadeIn(selected) {
+    Array.from(document.querySelectorAll(".fading-in")).forEach(el => {
         el.classList.remove("fading-in");
     })
     selected.classList.add("fading-in");
     selected.addEventListener("animationend", () => {
         selected.classList.remove("fading-in");
-        if (selected.id==interest){
+        if (selected.id == interest) {
             selected.classList.add("selected");
             startCountdownToChange();
-            
-            let nav=document.querySelector('.picture .navigation');
+
+            let nav = document.querySelector('.picture .navigation');
             nav.parentElement.removeChild(nav);
             selected.querySelector('.picture').appendChild(nav)
         }
@@ -124,15 +126,18 @@ function fadeIn(selected){
 
 }
 function selectInterestById(id) {
-    interest=id;
+    interest = id;
     clearTimeout(timeOut)
-    document.querySelector("#interests nav a.selected").classList.remove("selected");
+    let navSelected=document.querySelector("#interests nav a.selected");
+    if (navSelected) {
+        navSelected.classList.remove("selected");
+    }
     document.querySelector("a[data-interest-id=" + id + "]").classList.add("selected");
 
     let interestSection = document.querySelector("#interests article.selected")
     let selected = document.querySelector("#" + id)
-    if (interestSection){
-        if (interestSection.classList.contains("fading-out")){
+    if (interestSection) {
+        if (interestSection.classList.contains("fading-out")) {
             return;
         }
         interestSection.classList.add("fading-out");
@@ -140,11 +145,11 @@ function selectInterestById(id) {
             interestSection.classList.remove("selected");
             interestSection.classList.remove("fading-out");
             let selected = document.querySelector("#" + interest)
-           fadeIn(selected)
-        
+            fadeIn(selected)
+
         }, { once: true })
     } else {
         fadeIn(selected)
     }
-    
+
 }
