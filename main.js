f38ecb3a4d0a448fe49bfeda88e234fe0800fae6
Jan <crianonim@gmail.com>
2019-04-15 09:28:52 +0100

    Tidy up the JS code, correct comments, get rid of console.log

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 const INTERESTS_ID = ["languages", "botany", "cooking", "history", "technology"];
 const SECTIONS_ID = ["who", "why", "how"];
-const transitionTime=0.7;
-const interestViewTime=5;
+const transitionTime = 0.7;
+const interestViewTime = 5;
 let interest = "languages";
 let section = 0;
 let timeOutHandle;
 let paused = false;
-let sectionsSizes=[];
+let sectionsSizes = [];
 
 window.addEventListener("load", () => {
-    const body=document.body;
-    body.style.setProperty('--transition-time',transitionTime+"s");
-    body.style.setProperty('--transition-time-double',transitionTime*2+"s");
-    body.style.setProperty('--interest-view-time',interestViewTime+"s");
+
+    // all css variables
+    document.body.style.setProperty('--transition-time', transitionTime + "s");
+    document.body.style.setProperty('--transition-time-double', transitionTime * 2 + "s");
+    document.body.style.setProperty('--interest-view-time', interestViewTime + "s");
 
 
     // add data-interests-id to each nav button, keep the html clean
@@ -28,14 +29,13 @@ window.addEventListener("load", () => {
         changeSectionInHeaderNav(0)
     }
 
+    // interests nav button to change interest
     document.querySelector("#interests nav").addEventListener("click", (event) => {
         if (event.target.tagName == "A") selectInterestById(event.target.dataset.interestId)
     })
 
-    selectInterestById(interest)
-    // document.getElementById(interest).classList.add("selected");
 
-
+    // all keyboard bindings
     window.addEventListener("keydown", (event) => {
         if (event.keyCode == 37) {
             selectPreviousInterest();
@@ -43,12 +43,13 @@ window.addEventListener("load", () => {
         if (event.keyCode == 39) {
             selectNextInterest();
         }
-        if (event.keyCode==32){
+        if (event.keyCode == 32) {
             gotoNextSection();
             event.preventDefault();
         }
     })
 
+    // carousel buttons
     document.getElementById("pause-button").addEventListener("click", (event) => {
         event.target.innerHTML = paused ? '&#9208' : '&#9654'
         if (paused) resumeCountDown();
@@ -61,61 +62,63 @@ window.addEventListener("load", () => {
     document.getElementById("forward-button").addEventListener("click", () => {
         selectNextInterest();
     })
+
+    // control section based on scroll
     window.addEventListener("scroll", (event) => {
         let height = window.innerHeight;
         let scrolled = window.scrollY;
-        let newSection=getSectionIndexFromScroll(scrolled);
+        let newSection = getSectionIndexFromScroll(scrolled);
         if (newSection != section) {
-            // console.log("New section",newSection,window.innerHeight,scrolled)
             changeSectionInHeaderNav(newSection);
         }
     })
+
+    window.addEventListener("resize", (event) => {
+        calculateSectionSizes();
+    })
+
+    // kick off functionality
+    selectInterestById(interest)
     startCountdownToChange();
     calculateSectionSizes();
-
-    window.addEventListener("resize",(event)=>{
-       calculateSectionSizes();
-    })
 })
-function gotoNextSection(){
-   let destinationSection=Math.min(section+1,SECTIONS_ID.length-1);
-   let element=document.getElementById(SECTIONS_ID[destinationSection])
-   element.scrollIntoView({behavior: "smooth", block: "start"});
+
+function gotoNextSection() {
+    let destinationSection = Math.min(section + 1, SECTIONS_ID.length - 1);
+    let element = document.getElementById(SECTIONS_ID[destinationSection])
+    element.scrollIntoView({ behavior: "smooth", block: "start" });
 }
+
 // should be called whenever the size of viewport is changed;
-function calculateSectionSizes(){
-    sectionsSizes=SECTIONS_ID.map(id=>document.getElementById(id).clientHeight);
-//    console.log(sectionsSizes)
+function calculateSectionSizes() {
+    sectionsSizes = SECTIONS_ID.map(id => document.getElementById(id).clientHeight);
 }
-function getSectionIndexFromScroll(scroll,sizes=sectionsSizes){
-    let currentValue=0;
-    cummulativeSizes=sizes.map( el=> currentValue+=el );
-    let index=cummulativeSizes.findIndex( point=> scroll<point);
+function getSectionIndexFromScroll(scroll, sizes = sectionsSizes) {
+    let currentValue = 0;
+    cummulativeSizes = sizes.map(el => currentValue += el);
+    let index = cummulativeSizes.findIndex(point => scroll < point);
     return index;
-        
+
 }
 function changeSectionInHeaderNav(newSection) {
     let headerElement = document.querySelector('header');
     let sectionName = SECTIONS_ID[newSection]
     document.getElementById("nav-" + SECTIONS_ID[section]).classList.remove("selected")
-    // console.log("Change to",SECTIONS_ID[newSection]);
     document.getElementById("nav-" + sectionName).classList.add("selected")
     headerElement.classList.remove("who", "why", "how");
     headerElement.classList.add(sectionName);
-
     section = newSection;
 }
+
 function startCountdownToChange() {
     clearTimeout(timeOutHandle)
     timeOutHandle = setTimeout(() => {
-        // console.log("Happened!")
         if (paused) {
-            // console.log("Paused");
             return;
         }
         selectNextInterest();
         startCountdownToChange();
-    }, interestViewTime*1000);
+    }, interestViewTime * 1000);
 }
 function pauseCountDown() {
     clearTimeout(timeOutHandle);
@@ -125,8 +128,6 @@ function pauseCountDown() {
 function resumeCountDown() {
     paused = false;
     document.getElementById("interests").classList.remove("paused");
-
-    //maybe move to next already?
     startCountdownToChange();
 }
 function selectNextInterest() {
@@ -143,7 +144,6 @@ function selectInterestByStep(step) {
     if (current >= interestCount) current -= interestCount;
     if (current < 0) current += interestCount;
     selectInterestById(INTERESTS_ID[current])
-
 }
 
 function fadeIn(selected) {
@@ -156,7 +156,6 @@ function fadeIn(selected) {
         if (selected.id == interest) {
             selected.classList.add("selected");
             startCountdownToChange();
-
             let nav = document.querySelector('.picture .navigation');
             nav.parentElement.removeChild(nav);
             selected.querySelector('.picture').appendChild(nav)
@@ -167,7 +166,7 @@ function fadeIn(selected) {
 function selectInterestById(id) {
     interest = id;
     clearTimeout(timeOutHandle)
-    let navSelected=document.querySelector("#interests nav a.selected");
+    let navSelected = document.querySelector("#interests nav a.selected");
     if (navSelected) {
         navSelected.classList.remove("selected");
     }
@@ -185,7 +184,6 @@ function selectInterestById(id) {
             interestSection.classList.remove("fading-out");
             let selected = document.querySelector("#" + interest)
             fadeIn(selected)
-
         }, { once: true })
     } else {
         fadeIn(selected)
